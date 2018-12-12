4574c8f47f5e5d483d77f558bf9711a9cb1d1203
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2018-12-12 05:46:08 +0530

    added support for div display

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,15 +1,44 @@
-
 let startWithAppRegex = new RegExp('^app-','i');
-// document.addEventListener('dblclick',($event)=>{
-document.addEventListener('onmouseover',($event)=>{
-
+document.addEventListener('dblclick',($event)=>{
     let element = $event.target;
     while (!startWithAppRegex.test(element.tagName)){
         element = element.parentElement;
     }
-    element.style.backgroundColor = 'red';
     sendNgTag(element.tagName);
 });
+
+document.addEventListener('mouseover', ($event)=>{
+    if (!window.event.ctrlKey || !window.event.shiftKey ) return;
+    let element = $event.target;
+    while (!startWithAppRegex.test(element.tagName)){
+        element = element.parentElement;
+    }
+    removeMark();
+    let newMarkNode = createSpanNode(element.tagName);
+    element && element.appendChild(newMarkNode);
+    $event.stopPropagation();
+});
+
+function removeMark(){
+    try {
+        let oldMarkNode = document.getElementById('ng-bubble-mark');
+        oldMarkNode.parentElement.removeChild(oldMarkNode);
+    }catch (e) {
+        console.log(e);
+    }
+}
+
+function createSpanNode(text){
+    var span = document.createElement("p");
+    var t = document.createTextNode(text);
+    span.style.position = 'absolute';
+    span.style.top = 0;
+    span.style.left = 0;
+    span.style.fontSize = '8px';
+    span.id = 'ng-bubble-mark';
+    span.appendChild(t);
+    return span;
+}
 
 function sendNgTag(tag) {
     var xhttp = new XMLHttpRequest();
