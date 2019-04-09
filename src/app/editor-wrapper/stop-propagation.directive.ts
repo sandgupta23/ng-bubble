abc5d94c91bfb47aa806781bc82005145387d947
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-04-10 00:19:07 +0530

    fix(input): input hover fixes

---
+++ src/app/editor-wrapper/stop-propagation.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[stopPropagation]'
+})
+export class StopPropagationDirective {
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+}
