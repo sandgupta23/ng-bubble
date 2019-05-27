578fdc6632d03383a58d28f26b6b82e4317a2cbb
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-05-27 10:33:55 +0530

    fix: fixed loading indicator and json ordering

---
+++ src/app/sort-object.pipe.ts
@@ -0,0 +1,53 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {tap} from 'rxjs/operators';
+
+@Pipe({
+  name: 'objectByProbablePath'
+})
+export class ObjectByProbablePathPipe implements PipeTransform {
+
+  transform(object: object, path?: any): any {
+    if(!path || !path.trim()){
+      return object;
+    }
+    // let paths = path.trim().split('.');
+    let x = this.getParts(object, path);
+
+    let obj = {};
+    // let x = paths.reduce((total, path)=>{
+    //   let subTree = this.getSubObjectWithMatchedkey(total,path);
+    //   return {
+    //     ...total,
+    //     ...subTree
+    //   }
+    // },object);
+    return x;
+  }
+
+  getSubObjectWithMatchedkey(object, key){
+    return Object.keys(object).reduce((total:object, currentKey)=>{
+      if(currentKey.toLowerCase().includes(key.toLowerCase())){
+        total = {
+          ...total,
+          [currentKey]:object[currentKey]
+        };
+      }
+      return total
+    },{});
+  }
+
+  getParts(object, fragments) {
+    var [part, ...rest] = fragments.split('.');
+
+    return Object.assign({}, ...Object
+      .entries(object)
+      .filter(([key]) => key.toLowerCase().includes(part.toLowerCase()))
+      .map(([k, v]) => {
+        if (!rest.length) return { [k]: v };
+        var parts = v && typeof v === 'object' && this.getParts(v, rest.join('.'));
+        if (parts) return { [k]: parts };
+      })
+    );
+  }
+
+}
