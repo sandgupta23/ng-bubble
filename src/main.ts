8cbd8ff5387acfe36a91d07915a88e8aa631c88c
Sandeep Kumar Gupta <sandeepkumar.g@imimobile.com>
2019-06-10 21:11:38 +0530

    added setup and imports for unit and e2e testing

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-platformBrowserDynamic().bootstrapModule(AppModule, { ngZone: 'noop'})
+platformBrowserDynamic().bootstrapModule(AppModule, )
 // platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => {
     console.error(err);
